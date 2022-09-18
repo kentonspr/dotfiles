3ae26f7eb59b76f2a6fb5c1d03e345c58ef84e48
Kenton <kappanaitis@gmail.com>
2022-09-18 18:30:48 -0500

    Updates

---
+++ emacs/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a3010c151dc4f42d56dec26a85ae5640afc227bece71d058e394667718b66a49" default))
+ '(helm-minibuffer-history-key "M-p")
+ '(package-selected-packages
+   '(hydra helm-themes helm-swoop helm-rg helm-projectile helm which-key all-the-icons doom-modeline doom-themes general evil-collection evil no-littering auto-package-update diminish use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
