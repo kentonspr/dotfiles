d7dd764233313cb4f997420db7d30f44a5a23080
Kenton <kappanaitis@gmail.com>
2022-09-26 19:51:48 -0500

    Editing zsh config

--- zsh/zshrc.d/80-terraform.zsh
+++ zsh/zshrc.d/80-terraform.zsh
@@ -1,2 +1,9 @@
 autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C /usr/bin/terraform terraform
+
+alias tf="terraform"
+alias tfa="terraform apply"
+alias tfp="terraform plan"
+alias tfin="terraform init"
+alias tfim="terraform import"
+
