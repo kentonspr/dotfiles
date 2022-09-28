56c5f1c9fc3e6ae60b38b3da2a11667920234cfd
Kenton <kappanaitis@gmail.com>
2022-09-28 08:58:36 -0500

    More zsh changes

--- zsh/zshrc.d/99-prompt.zsh
+++ zsh/zshrc.d/99-prompt.zsh
@@ -1,7 +1,8 @@
 PROMPT="%{$fg[green]%}$USER@%m %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-PROMPT+=' %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+PROMPT+=' %{$fg[cyan]%}%c%{$reset_color%} $(git_super_status)'
+#PROMPT+='%B%m%~%b$(git_super_status) %# '
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+#ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+#ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+#ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+#ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
