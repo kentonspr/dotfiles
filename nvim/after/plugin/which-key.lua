7eac8aa9a663ad588c9483aac3116c4cf988ad10
Kenton <kappanaitis@gmail.com>
2023-04-11 13:40:24 -0500

    Changed minimum rows

--- nvim/after/plugin/which-key.lua
+++ nvim/after/plugin/which-key.lua
@@ -49,7 +49,7 @@ require("which-key").setup (
         winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
     },
     layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
+        height = { min = 1, max = 25 }, -- min and max height of the columns
         width = { min = 20, max = 50 }, -- min and max width of the columns
         spacing = 3, -- spacing between columns
         align = "left", -- align columns left, center or right
