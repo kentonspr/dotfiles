82f8e6d306bf3d3486757d056789432930c6ba84
Kenton Appanaitis <kappanaitis@gmail.com>
2023-04-24 13:11:01 -0500

    Fixed tab for now

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -53,11 +53,11 @@ cmp.setup({
             if cmp.visible() then
                 cmp.select_next_item()
                 -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                -- they way you will only jump inside the snippet region
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
+                -- this way you will only jump inside the snippet region
+                -- elseif luasnip.expand_or_jumpable() then
+                --     luasnip.expand_or_jump()
+                -- elseif has_words_before() then
+                --     cmp.complete()
             else
                 fallback()
             end
@@ -65,8 +65,8 @@ cmp.setup({
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+                -- elseif luasnip.jumpable(-1) then
+                --     luasnip.jump(-1)
             else
                 fallback()
             end
