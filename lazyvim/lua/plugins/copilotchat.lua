f8ed664c55b402bbf9a25713a598bb4556d5b87d
Kenton Appanaitis <kappanaitis@gmail.com>
2024-02-22 15:35:30 -0600

    More key bindings.

--- lazyvim/lua/plugins/copilotchat.lua
+++ lazyvim/lua/plugins/copilotchat.lua
@@ -18,11 +18,40 @@ return {
     vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
   end,
   event = "VeryLazy",
+  dependencies = {
+    { "nvim-telescope/telescope.nvim" }, -- Use telescope for help actions
+    { "nvim-lua/plenary.nvim" },
+  },
   keys = {
     { "<leader>ccc", ":CopilotChat ", desc = "CopilotChat - Prompt" },
     { "<leader>cce", ":CopilotChatExplain ", desc = "CopilotChat - Explain code" },
+    { "<leader>cci", ":CopilotChatFixDiagnostic<cr>", desc = "CopilotChatFixDiagnostic" },
+    { "<leader>cci", ":CopilotChatInPlace<cr>", desc = "CopilotChatInPlace" },
     { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
     { "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
     { "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
+    { "<leader>ccv", ":CopilotChatVsplitToggle<cr>", desc = "CopilotChatVsplitToggle" },
+    -- Show help actions with telescope
+    {
+      "<leader>cch",
+      function()
+        require("CopilotChat.code_actions").show_help_actions()
+      end,
+      desc = "CopilotChat - Help actions",
+    },
+    -- Show prompts actions with telescope
+    {
+      "<leader>ccp",
+      function()
+        require("CopilotChat.code_actions").show_prompt_actions()
+      end,
+      desc = "CopilotChat - Help actions",
+    },
+    {
+      "<leader>ccp",
+      ":lua require('CopilotChat.code_actions').show_prompt_actions(true)<CR>",
+      mode = "x",
+      desc = "CopilotChat - Prompt actions",
+    },
   },
 }
