b9d6752c3195e7df9daee4e6635b24f13f29722a
Kenton Appanaitis <kappanaitis@gmail.com>
2024-02-22 10:28:06 -0600

    Line number color changes

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -12,3 +12,10 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.g.python3_host_prog = "/home/kenton/.pyenv/versions/nvim/bin/python"
+
+-- Sets colors to line numbers Above, Current and Below  in this order
+-- vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC' })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#adadad" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
+-- vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F' })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#adadad" })
