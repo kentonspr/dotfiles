2b3d4cb4b9de510ae50574c00d8c42848701d923
Kenton Appanaitis <kappanaitis@gmail.com>
2024-03-28 10:07:58 -0500

    Changed to basedpyright

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,3 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additional options here-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basedpyright"
--- lazyvim/lua/plugins/mason.lua
+++ lazyvim/lua/plugins/mason.lua
@@ -2,6 +2,7 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
+      "basedpyright",
       "stylua",
       "shellcheck",
       "shfmt",
