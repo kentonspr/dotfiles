5beb129912018e898c42bdfe2c8c9c41af685b2f
Kenton Appanaitis <kappanaitis@gmail.com>
2024-07-19 17:30:12 -0500

    Changes

--- lazyvim/lua/plugins/which-key.lua
+++ lazyvim/lua/plugins/which-key.lua
@@ -1,11 +1,11 @@
 return {
   "folke/which-key.nvim",
   opts = {
-    defaults = {
-      ["<leader>cc"] = { name = "+copilot" },
-      ["<leader>t"] = { name = "+tabs" },
-      ["<leader>tn"] = { name = "+new" },
-      ["<leader>tm"] = { name = "+move" },
+    spec = {
+      { "<leader>cc", group = "+copilot" },
+      { "<leader>t", group = "+tabs" },
+      { "<leader>tn", group = "+new" },
+      { "<leader>tm", group = "+move" },
     },
   },
 }
